fd13d5e7b4921ef24dacd72a13655143347ee934
jimenaamr <202105740@alu.comillas.edu>
2025-05-04 20:48:38 +0200

    primer intento

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -8,9 +8,9 @@ terraform {
 }
 
 provider "google" {
-  credentials = file("../../icai2025-5f9879654f17.json")
+  credentials = file("../../examen-458207-0e812b386d42.json")
 
-  project = 
-  region  = 
-  zone    = 
+  project = var.gcp-project
+  region  = var.gcp-region
+  zone    = var.gcp-zone
 }
